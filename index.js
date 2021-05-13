91686b78a89f37f0d97c88996f0bbd088a44467a
telvinvarfley <telvinvarfley@gmail.com>
2021-05-13 23:43:17 +0000

    Major revamp

--- index.js
+++ index.js
@@ -9,3 +9,26 @@
      hamburger.classList.toggle("active");
      navMenu.classList.toggle("active");
  }
+
+// //  Sticky nav bar
+
+
+// let sticky = navbar.offsetTop;
+
+// function scrollFunction(){
+//     if(window.pageYOffset>= sticky){
+//         navBar.classList.add("sticky")
+//     }else{
+//         navBar.classList.remove("sticky");
+//     }
+// }
+
+// window.onscroll = function(){
+//     scrollFunction();
+// }
+
+// window.addEventListener("scroll", ()=>{
+// const navBar = document.querySelector(".navbar");
+  
+//     navBar.classList.toggle("sticky", window.scrollY > 0 )
+// })
